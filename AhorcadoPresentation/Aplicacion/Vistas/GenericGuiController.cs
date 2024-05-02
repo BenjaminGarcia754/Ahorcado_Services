69a3b5be0fccb8780c20a47f0c6c72e6255a3760
Benjamin Garcia <linkdelira@gmail.com>
2024-05-01 19:03:22 -0600

    Actualizado Modelo de usuario TODO: Generar GUI iniciar Sesion y agregar movimiento al Menu principal

---
+++ AhorcadoPresentation/Aplicacion/Vistas/GenericGuiController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Channels;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AhorcadoPresentation.Aplicacion.Vistas
+{
+    internal static class GenericGuiController
+    {
+        public static void MostrarMensajeBox(string mensaje)
+        {
+            MessageBox.Show(mensaje);
+        }
+    }
+}
