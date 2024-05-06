ad4afa141f0d94c91f97d86fad71aac7242f60bc
Benjamin Garcia <linkdelira@gmail.com>
2024-05-06 13:54:37 -0600

    TODO:Modificar Controller RegistrarUsuario.cs

--- AhorcadoPresentation/Aplicacion/Vistas/RegistrarUsuario.xaml.cs
+++ AhorcadoPresentation/Aplicacion/Vistas/RegistrarUsuario.xaml.cs
@@ -24,5 +24,20 @@ namespace AhorcadoPresentation.Aplicacion.Vistas
         {
             InitializeComponent();
         }
+
+        public static bool esRegistro { get; set; }
+
+        private void ClickRegistrarse(object sender, RoutedEventArgs e)
+        {
+            if (ValidarDatos())
+            {
+
+            }
+        }
+
+        private void ClickRegresar(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
