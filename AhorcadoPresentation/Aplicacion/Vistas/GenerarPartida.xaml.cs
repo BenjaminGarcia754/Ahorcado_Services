04de2e38da6b07698a377352061fdf3d47624838
Caixba22 <miguelmtzcaixba@outlook.com>
2024-05-14 01:01:32 -0600

    CAmbios en generarPartida, creación de IDificultad

--- AhorcadoPresentation/Aplicacion/Vistas/GenerarPartida.xaml.cs
+++ AhorcadoPresentation/Aplicacion/Vistas/GenerarPartida.xaml.cs
@@ -1,4 +1,6 @@
-﻿using JugadorServiceReference;
+﻿using AhorcadoService;
+using JugadorServiceReference;
+using ServiceReference1;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,7 +27,48 @@ namespace AhorcadoPresentation.Aplicacion.Vistas
         public GenerarPartida()
         {
             InitializeComponent();
+            InicializarComboBoxDificultades();
+            InicializarComboBoxCategorias();
 
+            // Suscribir al evento SelectionChanged del ComboBox de dificultades
+            CbDificultad.SelectionChanged += (sender, e) =>
+            {
+                // Habilitar el ComboBox de categorías cuando se seleccione una dificultad
+                CbCategoria.IsEnabled = CbDificultad.SelectedItem != null;
+            };
+
+            // Suscribir al evento SelectionChanged del ComboBox de categorías
+            CbCategoria.SelectionChanged += (sender, e) =>
+            {
+                // Habilitar el ComboBox de palabras cuando se seleccione una categoría
+                CbPalabra.IsEnabled = CbCategoria.SelectedItem != null;
+            };
+        }
+
+        private void InicializarComboBoxDificultades()
+        {
+            var dificultadServiceClient = new DificultadServiceClient();
+            var dificultades = dificultadServiceClient.GetDificultadesAsync().Result;
+
+            CbDificultad.ItemsSource = dificultades;
+            CbDificultad.DisplayMemberPath = "Nombre";
+            CbDificultad.SelectedValuePath = "Id";
+        }
+
+        private void InicializarComboBoxCategorias()
+        {
+            var categoriaServiceClient = new CategoriaServiceClient();
+            var categorias = categoriaServiceClient.GetCategoriasAsync().Result;
+
+            CbCategoria.ItemsSource = categorias;
+            CbCategoria.DisplayMemberPath = "Nombre";
+            CbCategoria.SelectedValuePath = "Id";
+
+            // Inicialmente bloquear el ComboBox de categorías
+            CbCategoria.IsEnabled = false;
+
+            // Inicialmente bloquear el ComboBox de palabras
+            CbPalabra.IsEnabled = false;
         }
 
         private void Click_CrearSala(object sender, RoutedEventArgs e)
