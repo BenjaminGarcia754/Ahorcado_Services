8fd0726fdda6123f9b0f7734303ae11d9e6f511c
CarlosRodriguezJr <100862307+CarlosRodriguezJr@users.noreply.github.com>
2024-05-16 17:39:30 -0600

    VISTAS CARLOS

---
+++ AhorcadoPresentation/Aplicacion/Vistas/Historial.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AhorcadoPresentation.Aplicacion.Vistas
+{
+    /// <summary>
+    /// Interaction logic for Historial.xaml
+    /// </summary>
+    public partial class Historial : UserControl
+    {
+        public Historial(string fecha, string usuarioContrincante, string palabra, string resultado, string puntaje)
+        {
+            InitializeComponent();
+            lFechaPartida.Content = fecha;
+            lUsuarioContrincante.Content = usuarioContrincante;
+            lPalabraPartida.Content = palabra;
+            lResultado.Content = resultado;
+            lPuntaje.Content = puntaje;
+        }
+    }
+}
---
+++ AhorcadoPresentation/Aplicacion/Vistas/HistorialPuntos.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AhorcadoPresentation.Aplicacion.Vistas
+{
+    /// <summary>
+    /// Interaction logic for HistorialPuntos.xaml
+    /// </summary>
+    public partial class HistorialPuntos : UserControl
+    {
+        public HistorialPuntos(string fecha, string usuarioContrincante, string palabra, string puntaje)
+        {
+            InitializeComponent();
+            lFechaPartida.Content = fecha;
+            lUsuarioContrincante.Content = usuarioContrincante;
+            lPalabraPartida.Content = palabra;
+            lPuntaje.Content = puntaje;
+        }
+    }
+}
