d46487dbb74e34492876e1dd53de38d44d96ee65
Caixba22 <miguelmtzcaixba@outlook.com>
2024-05-17 10:02:41 -0600

    Vista BuscarPartidaNueva

---
+++ AhorcadoPresentation/Aplicacion/Vistas/PartidaNueva.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AhorcadoPresentation.Aplicacion.Vistas
+{
+    /// <summary>
+    /// Lógica de interacción para PartidaNueva.xaml
+    /// </summary>
+    public partial class PartidaNueva : UserControl
+    {
+        public PartidaNueva(string jugadorRetador, string dificultad, string categoria)
+        {
+            InitializeComponent();
+            lJugadorRetador.Content = jugadorRetador;
+            lDificultad.Content = dificultad;
+            lCategoria.Content = categoria;
+        }
+    }
+}
