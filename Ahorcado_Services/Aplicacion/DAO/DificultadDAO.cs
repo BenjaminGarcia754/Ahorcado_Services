115472afc587bcc42f43fa26295630b65c0a8c29
Benjamin Garcia <linkdelira@gmail.com>
2024-05-22 19:30:11 -0600

    Agregado servicio para dificultad

--- Ahorcado_Services/Aplicacion/DAO/DificultadDAO.cs
+++ Ahorcado_Services/Aplicacion/DAO/DificultadDAO.cs
@@ -1,8 +1,10 @@
 ﻿using Ahorcado_Services.Infraestructura.Utilidades;
 using Ahorcado_Services.Modelo.EntityFramework;
 using AutoMapper;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 
@@ -17,6 +19,32 @@ namespace Ahorcado_Services.Aplicacion.DAO
         public static List<Dificultad> GetDificultades()
         {
             return ahorcadoDbContext.Dificultades.ToList();
+        }
+
+        public static Dificultad GetDificultad(int id)
+        {
+            return ahorcadoDbContext.Dificultades.Find(id);
+        }
+
+        public static bool AddDificultad(Dificultad dificultad)
+        {
+            bool respuesta = true;
+            try
+            {
+                ahorcadoDbContext.Dificultades.Add(dificultad);
+                ahorcadoDbContext.SaveChanges();
+            }
+            catch (DbUpdateException ex)
+            {
+                Console.WriteLine(ex.Message);
+                respuesta = false;
+            }
+            catch (EntityException ex)
+            {
+                Console.WriteLine(ex.Message);
+                respuesta = false;
+            }
+            return respuesta;
         }
     }
 }
